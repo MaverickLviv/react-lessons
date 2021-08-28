dd4ceab2d2429ca1ffdae0c3651e408cec255e0b
MaverickLviv <pkrainikov@gmail.com>
2021-08-28 11:05:28 +0300

    4 HomeWork

--- src/service/user.api.service.js
+++ src/service/user.api.service.js
@@ -2,12 +2,12 @@
 const saveUser = (user) => {
     fetch('https://jsonplaceholder.typicode.com/users', {
         method: 'POST',
-        body: JSON.stringify({user}),
+        body: JSON.stringify(user),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
-    }) /*Promise <Response>*/
-        .then((response: Response) => response.json()) /*Promise <any>*/
+    })
+        .then((response) => response.json())
         .then((json) => console.log(json));
 }
 
